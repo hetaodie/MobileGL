fbc308edf0dfcc5f138c3c879a6b4d5a338e02f8
weixu <weixu6130@163.com>
2019-05-21 19:20:29 +0800

    测试opengl图片轮播

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -92,6 +92,12 @@ void Renderer::render(){
     GLint textureLocation = glGetAttribLocation(mShaderProgram->mProgram, "coord");
     glEnableVertexAttribArray(textureLocation);
     glVertexAttribPointer(textureLocation, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (char *)(6 * sizeof(float)));
+    
+    GLuint moveU = glGetUniformLocation(mShaderProgram->mProgram, "move1");
+    glUniform1f(moveU, addX);
+    //    glVertexAttrib1f(moveLocation, 0.1);
+    
+    
     std::vector<GLuint>::iterator iVector = mTextureVector.begin();
     int num = 0;
     while(iVector != mTextureVector.end())
@@ -110,13 +116,13 @@ void Renderer::render(){
 
     glm::mat4 trans;
 //    trans = glm::translate(trans, glm::vec3(addX, addy, 0.0f));
-    long time = getCurrentTime();
-    float angle = (time % 360) / 10;
-    printf("weixu = %f \n", angle);
-    trans = glm::rotate(trans,addX, glm::vec3(1.0f, 1.0f, 0.0f));
-    
-    GLuint transformLoc = glGetUniformLocation(mShaderProgram->mProgram, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+//    long time = getCurrentTime();
+//    float angle = (time % 360) / 10;
+//    printf("weixu = %f \n", angle);
+//    trans = glm::rotate(trans,addX, glm::vec3(1.0f, 1.0f, 0.0f));
+//
+//    GLuint transformLoc = glGetUniformLocation(mShaderProgram->mProgram, "transform");
+//    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
     mShaderProgram->useProgram();
     if (mRenderData.index.size() > 0) {
@@ -125,8 +131,8 @@ void Renderer::render(){
         GLsizei num = (GLsizei)mRenderData.mVertices.size()/mRenderData.vertexNum;
         glDrawArrays(GL_TRIANGLES, 0, num);
     }
-    addX += 1;
-    if (addX > 360) {
+    addX += 0.01;
+    if (addX > 1) {
         addX = 0;
     }
 }
@@ -151,8 +157,8 @@ GLuint createTexture2D(GLenum format, int width, int height, void *data)
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
     glBindTexture(GL_TEXTURE_2D, 0);
     return texture;
